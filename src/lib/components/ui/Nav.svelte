9c9db89bda6f4483cf838035a38217b4daca58b9
Nate Houk <nate@epsilonrecords.com>
2025-01-13 18:04:05 +0100

    Refactor

--- src/lib/components/ui/Nav.svelte
+++ src/lib/components/ui/Nav.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-
+  import UserButton from 'clerk-sveltekit/client/UserButton.svelte';
+  import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
+  import SignedOut from 'clerk-sveltekit/client/SignedOut.svelte';
 </script>
 
 <nav class="absolute top-0 w-full z-20">
@@ -7,12 +9,27 @@
     <div class="flex justify-between items-center py-6">
       <a href="/" class="text-white font-mono font-bold text-xl">EPSILON</a>
       <ul class="flex gap-8">
-        <li><a href="/releases" class="hover:text-red-400 text-white transition-colors">Releases</a></li>
-        <li><a href="/artists" class="hover:text-red-400 text-white transition-colors">Artists</a></li>
-        <li><a href="/events" class="hover:text-red-400 text-white transition-colors">Events</a></li>
+        <li>
+          <a href="/releases" class="hover:text-red-400 text-white transition-colors">Releases</a>
+        </li>
+        <li>
+          <a href="/artists" class="hover:text-red-400 text-white transition-colors">Artists</a>
+        </li>
+        <li>
+          <a href="/events" class="hover:text-red-400 text-white transition-colors">Events</a>
+        </li>
         <li><a href="/shop" class="hover:text-red-400 text-white transition-colors">Shop</a></li>
-        <li><a href="/contact" class="hover:text-red-400 text-white transition-colors">Contact</a></li>
-        <li><a href="/login" class="hover:text-red-400 text-white transition-colors">Login</a></li>
+        <li>
+          <a href="/contact" class="hover:text-red-400 text-white transition-colors">Contact</a>
+        </li>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+        <SignedOut>
+          <li>
+            <a href="/login" class="hover:text-red-400 text-white transition-colors">Login</a>
+          </li>
+        </SignedOut>
       </ul>
     </div>
   </div>
