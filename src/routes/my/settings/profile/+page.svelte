db8470810cdd9b6b63aebbbfaedf34917ad52cc4
Nate Houk <nate@epsilonrecords.com>
2025-01-14 14:05:13 +0100

    Refactor

--- src/routes/my/settings/profile/+page.svelte
+++ src/routes/my/settings/profile/+page.svelte
@@ -8,12 +8,15 @@
   <form action="?/update" method="POST" class="flex flex-col space-y-2 w-full">
     <h3 class="text-2xl font-medium">Update Profile</h3>
     <div class="form-control w-full max-w-lg">
-      <label for="avatar" class="label font-medium pb-1">Profile Picture</label>
+      <label for="avatar" class="label font-medium pb-1">
+        <span class="label-text">Profile Picture</span>
+      </label>
+      <label for="avatar" class="avatar w-32 rounded-full">
+        <div class="w-32 rounded-full">
+          <img src="https://placeimg.com/80/80/people" alt="user avatar" />
+        </div>
+      </label>
+      <input type="file" name="avatar" id="avatar" value="" accept="image/*" hidden />
     </div>
-    <label for="avatar" class="avatar w-32 rounded-full">
-      <div class="w-32 rounded-full">
-        <img src="https://placeimg.com/80/80/people" alt="user avatar" />
-      </div>
-    </label>
   </form>
 </div>
