9f393e885de75eb0ab417da60ca6e9294f3cc416
Nate Houk <nate@epsilonrecords.com>
2025-01-14 14:03:01 +0100

    Refactor

--- src/routes/my/settings/profile/+page.svelte
+++ src/routes/my/settings/profile/+page.svelte
@@ -7,5 +7,13 @@
 <div class="flex flex-col w-full h-full">
   <form action="?/update" method="POST" class="flex flex-col space-y-2 w-full">
     <h3 class="text-2xl font-medium">Update Profile</h3>
+    <div class="form-control w-full max-w-lg">
+      <label for="avatar" class="label font-medium pb-1">Profile Picture</label>
+    </div>
+    <label for="avatar" class="avatar w-32 rounded-full">
+      <div class="w-32 rounded-full">
+        <img src="https://placeimg.com/80/80/people" alt="user avatar" />
+      </div>
+    </label>
   </form>
 </div>
