5837cd0a27efa38ffbd2211cb4c90313bccc2765
Nate Houk <nate@epsilonrecords.com>
2025-01-15 10:55:24 +0100

    Refactor

--- src/routes/my/settings/profile/+page.svelte
+++ src/routes/my/settings/profile/+page.svelte
@@ -124,7 +124,7 @@
   >
     <div class="space-y-4">
       <h3 class="text-2xl font-semibold">Select Artist</h3>
-      <div class="border border-neutral-900 bg-white rounded-lg">
+      <div class="border border-neutral-900 bg-white rounded-lg max-w-lg">
         <OrganizationSwitcher
           appearance={{ baseTheme: $mode === 'dark' ? dark : neobrutalism }}
           hidePersonal={true}
