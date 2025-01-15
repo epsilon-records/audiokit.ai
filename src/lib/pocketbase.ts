472517716e02b885f5486b5ce158b2fe68be84c2
Nate Houk <nate@epsilonrecords.com>
2025-01-15 21:39:48 +0100

    Refactor

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,4 +1,6 @@
 import PocketBase from 'pocketbase';
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import { PB_SECRET_KEY } from '$env/static/private';
 
 export const pb = new PocketBase(PUBLIC_POCKETBASE_URL)
+await pb.collection("users").authWithPassword('pb@epsilonrecords.com', PB_SECRET_KEY);
