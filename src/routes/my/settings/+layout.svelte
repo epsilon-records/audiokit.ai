f89136f1cc22c17b80b48457b32ded1d06167f62
Nate Houk <nate@epsilonrecords.com>
2025-01-15 09:25:55 +0100

    Refactor

--- src/routes/my/settings/+layout.svelte
+++ src/routes/my/settings/+layout.svelte
@@ -2,13 +2,22 @@
   import PageContainer from '$lib/components/PageContainer.svelte';
   import SettingsNav from '$lib/components/SettingsNav.svelte';
   import { OrganizationSwitcher } from 'svelte-clerk';
+  import { mode } from 'mode-watcher';
+  import { neobrutalism, dark } from '@clerk/themes';
 
   let { children } = $props();
 </script>
 
 <PageContainer title="Settings" bgColor="bg-green-100" textColor="text-foreground">
   <div class="flex w-full h-full space-x-4">
-    <OrganizationSwitcher />
+    <div class="pt-2">
+      <OrganizationSwitcher
+        appearance={{ baseTheme: $mode === 'dark' ? dark : neobrutalism }}
+        hidePersonal={true}
+        afterCreateOrganizationUrl="/profile"
+        afterSelectOrganizationUrl="/profile"
+      />
+    </div>
     <SettingsNav />
     <div class="w-full">
       {@render children()}
