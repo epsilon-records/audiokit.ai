ceec879a5502eec39053f2875aafd389d7abf7ed
Nate Houk <nate@epsilonrecords.com>
2025-01-15 21:03:51 +0100

    Refactor

--- src/routes/my/settings/profile/+page.server.ts
+++ src/routes/my/settings/profile/+page.server.ts
@@ -1,14 +1,16 @@
 import { message, superValidate } from 'sveltekit-superforms/server';
 import { zod } from 'sveltekit-superforms/adapters';
-import { error, fail } from '@sveltejs/kit';
+import { error, fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { pb } from '$lib/pocketbase';
 import { artistSchema } from '$lib/schemas/artist';
 
 export const load = (async ({ locals }) => {
-	if (!locals.auth?.userId || !locals.auth?.orgId) {
-        throw error(401, 'Unauthorized');
-    }
+	if (!locals.auth?.userId) {
+		throw error(401, 'Unauthorized');
+	} else if (!locals.auth?.orgId) {
+		redirect(302, '/my/settings/create');
+	}
 	const artists = await pb.collection('artists').getList(1, 1, {
 		filter: `org_id = "${locals.auth.orgId}" || test_org_id = "${locals.auth.orgId}"`,
 	});
