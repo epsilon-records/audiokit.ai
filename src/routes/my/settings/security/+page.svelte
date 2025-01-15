4d3e92d7eaa62a5a150cf78e2ad3427e53c9b6fa
Nate Houk <nate@epsilonrecords.com>
2025-01-15 01:27:10 +0100

    Refactor

--- src/routes/my/settings/security/+page.svelte
+++ src/routes/my/settings/security/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { PageData } from './$types';
+  import SettingsContainer from '$lib/components/SettingsContainer.svelte';
 
   let { data }: { data: PageData } = $props();
 </script>
