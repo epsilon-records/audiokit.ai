3ca720e94082c7566f39d0a6688886bda1ee3674
Nate Houk <nate@epsilonrecords.com>
2025-01-15 21:50:32 +0100

    Refactor

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,11 @@
 import { withClerkHandler } from 'svelte-clerk/server';
-import { initPocketBase } from '$lib/pocketbase';
+import { pb } from '$lib/pocketbase';
+import { PB_SECRET_KEY } from '$env/static/private';
 import { sequence } from '@sveltejs/kit/hooks';
-import type { Handle } from '@sveltejs/kit';
 
-const pocketbaseHandle: Handle = async ({ event, resolve }) => {
-    await initPocketBase();
-    return await resolve(event);
-};
+export async function pocketbaseHandler({ event, resolve }: { event: any; resolve: any }) {
+    await pb.collection("users").authWithPassword('pb@epsilonrecords.com', PB_SECRET_KEY);
+    return resolve(event);
+}
 
-export const handle = sequence(withClerkHandler(), pocketbaseHandle);
+export const handle = sequence(withClerkHandler(), pocketbaseHandler);
--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,9 +1,4 @@
 import PocketBase from 'pocketbase';
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
-import { PB_SECRET_KEY } from '$env/static/private';
 
 export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
-
-export async function initPocketBase() {
-    await pb.collection("users").authWithPassword('pb@epsilonrecords.com', PB_SECRET_KEY);
-}
