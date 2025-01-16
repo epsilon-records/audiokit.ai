2dad3f7adc9393ab3733a0a1ac7cb2078f40f589
Nate Houk <nate@epsilonrecords.com>
2025-01-16 11:26:16 +0100

    Reactor

--- src/lib/components/PageContainer.svelte
+++ src/lib/components/PageContainer.svelte
@@ -12,7 +12,7 @@
   }>();
 </script>
 
-<div class="min-h-screen py-24">
+<div class="min-h-screen py-24 {bgColor}">
   <div class="container mx-auto px-4">
     {#if title}
       <h1 class="text-4xl font-bold {textColor} mb-12">{title}</h1>
