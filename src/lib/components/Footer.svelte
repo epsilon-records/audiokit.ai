2eb5efcd285f29a8ac6e9932798d4a314644ec5e
Nate Houk <nate@epsilonrecords.com>
2025-01-16 20:11:14 +0100

    Refactor

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -59,7 +59,7 @@
       <!-- Social -->
       <div>
         <h3 class="font-bold mb-4">Follow Us</h3>
-        <div class="grid grid-cols-5 gap-4">
+        <div class="grid grid-cols-4 gap-4">
           <a
             href="https://beatport.com/label/epsilon-records/112442"
             class="hover:opacity-80 transition-opacity"
