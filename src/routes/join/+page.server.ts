a7ff25a9e7024eb05076e59d0514da43f8ffdcc1
Nate Houk <nate@epsilonrecords.com>
2025-01-20 13:00:28 +0100

    Refactor

--- src/routes/join/+page.server.ts
+++ src/routes/join/+page.server.ts
@@ -1,14 +1,14 @@
 import type { PageServerLoad } from './$types';
 import { clerkClient } from 'svelte-clerk/server';
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals }) => {
-  if (locals.auth?.userId) {
-    const user = await clerkClient.users.getUser(locals.auth.userId);
-    return {
-      email: user.primaryEmailAddress?.emailAddress ?? null,
-    };
+  if (!locals.auth?.userId) {
+    throw redirect(303, '/pricing');
   }
+
+  const user = await clerkClient.users.getUser(locals.auth.userId);
   return {
-    email: null,
+    email: user.primaryEmailAddress?.emailAddress,
   };
 };
