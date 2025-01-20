0931d149e279f544df0a06575953aea66ca0afff
Nate Houk <nate@epsilonrecords.com>
2025-01-20 20:22:06 +0100

    Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,38 +10,6 @@
   let { children } = $props<{ children: Snippet }>();
 </script>
 
-<svelte:head>
-  {#if !import.meta.env.PROD}
-    <script
-      data-recording-token="YlK6PCQzlht5rZutE1qns77yPUiS5FNPT6QBMXM0"
-      data-is-production-environment="false"
-      src="https://snippet.meticulous.ai/v1/meticulous.js"
-    ></script>
-    <script>
-      (function () {
-        var dbpr = 100;
-        if (Math.random() * 100 > 100 - dbpr) {
-          var d = 'dbbRum',
-            w = window,
-            o = document,
-            a = addEventListener,
-            scr = o.createElement('script');
-          scr.async = !0;
-          w[d] = w[d] || [];
-          w[d].push(['presampling', dbpr]);
-          ['error', 'unhandledrejection'].forEach(function (t) {
-            a(t, function (e) {
-              w[d].push([t, e]);
-            });
-          });
-          scr.src = 'https://cdn.debugbear.com/5GOULW9zQSMv.js';
-          o.head.appendChild(scr);
-        }
-      })();
-    </script>
-  {/if}
-</svelte:head>
-
 <ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
   <div class="min-h-screen flex flex-col">
     <Nav />
