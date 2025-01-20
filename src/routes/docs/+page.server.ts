e6de7ecf8ccc98c96abe35a77a238163685a5789
Nate Houk <nate@epsilonrecords.com>
2025-01-20 12:22:36 +0100

    Refactor

--- src/routes/docs/+page.server.ts
+++ src/routes/docs/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-  throw redirect(307, 'https://epsilon.readme.io');
+  throw redirect(307, 'https://docs.audiokit.ai');
 };
