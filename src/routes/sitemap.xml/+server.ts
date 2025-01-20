5b3c44ac6df12c55b8a54f4f7d7e98a523e8e40e
Nate Houk <nate@epsilonrecords.com>
2025-01-20 20:47:51 +0100

    Refactor

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,11 +1,12 @@
-import { PUBLIC_ORIGIN } from '$env/static/public';
-import * as sitemap from 'super-sitemap';
 import type { RequestHandler } from '@sveltejs/kit';
+import * as sitemap from 'super-sitemap';
+import { PUBLIC_ORIGIN } from '$env/static/public';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const prerender = true; // optional
+
+export const GET: RequestHandler = async () => {
   return await sitemap.response({
     origin: PUBLIC_ORIGIN,
-    page: params.page,
     excludeRoutePatterns: [
       '^/dashboard.*', // i.e. routes starting with `/dashboard`
     ],
