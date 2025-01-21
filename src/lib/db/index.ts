0a5e1bf11d5d4d7713f79e6ba828b3c805498c2f
Nate Houk <nate@epsilonrecords.com>
2025-01-21 01:13:52 +0100

    Refactor

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { SUPABASE_DATABASE_URL } from '$env/static/private';
+import { DATABASE_URL } from '$env/static/private';
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(SUPABASE_DATABASE_URL, { prepare: false, ssl: 'require' });
+const client = postgres(DATABASE_URL, { prepare: false, ssl: 'require' });
 export const db = drizzle({ client });
