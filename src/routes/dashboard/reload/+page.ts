7975fca55addaa70c2fbd00b8b3a3a4b15517f84
Nate Houk <nate@epsilonrecords.com>
2025-01-22 06:05:43 +0100

    Refactor

--- src/routes/dashboard/reload/+page.ts
+++ src/routes/dashboard/reload/+page.ts
@@ -1,11 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import { invalidateAll } from '$app/navigation';
-import { browser } from '$app/environment';
 
 export const load = async () => {
-  if (browser) {
-    await invalidateAll();
-    throw redirect(307, '/dashboard');
-  }
+  await invalidateAll();
   throw redirect(307, '/dashboard');
 };
