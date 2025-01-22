f1bad3e0b1883a0394bd48a9197ade3ccd52388a
Nate Houk <nate@epsilonrecords.com>
2025-01-22 10:20:58 +0100

    Refactor

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,14 @@
-import * as Sentry from '@sentry/sveltekit';
+// import * as Sentry from '@sentry/sveltekit';
 import { withClerkHandler } from 'svelte-clerk/server';
 import { sequence } from '@sveltejs/kit/hooks';
 import '@stripe/stripe-js';
 
-Sentry.init({
-  dsn: 'https://b89f1762b68462bece4cd38d79eca72f@o337159.ingest.us.sentry.io/4508676068540416',
-  tracesSampleRate: 1,
-});
+// Sentry.init({
+//   dsn: 'https://b89f1762b68462bece4cd38d79eca72f@o337159.ingest.us.sentry.io/4508676068540416',
+//   tracesSampleRate: 1,
+// });
 
-export const handle = sequence(Sentry.sentryHandle(), sequence(withClerkHandler()));
-export const handleError = Sentry.handleErrorWithSentry();
+// export const handle = sequence(Sentry.sentryHandle(), withClerkHandler());
+// export const handleError = Sentry.handleErrorWithSentry();
+
+export const handle = withClerkHandler();
