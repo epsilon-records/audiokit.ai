75db7ce103e27635ebb751d742d616f18dd0739c
Nate Houk <nate@epsilonrecords.com>
2025-01-23 11:45:06 +0100

    Refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,6 @@
-// import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  resolve: process.env.VITEST
-    ? {
-        conditions: ['browser'],
-      }
-    : undefined,
-  plugins: [
-    // sentrySvelteKit({
-    //   sourceMapsUploadOptions: {
-    //     org: 'epsilon-records',
-    //     project: 'audiokit',
-    //   },
-    // }),
-    sveltekit(),
-    visualizer({
-      emitFile: true,
-      filename: 'stats.html',
-    }),
-  ],
+  plugins: [sveltekit()],
 });
