0510126ee35d29fb2cf08cca80a481486530f188
Nate Houk <nate@epsilonrecords.com>
2025-01-26 00:35:22 +0100

    Refactor

--- src/lib/components/hero/CtaButtons.svelte
+++ src/lib/components/hero/CtaButtons.svelte
@@ -9,6 +9,6 @@
   in:fly={{ y: 30, duration: 800, delay: ANIMATION_BASE_DELAY + 200 }}
   class="flex flex-col sm:flex-row gap-4"
 >
-  <Button size="lg" class="text-lg px-8" href="/sign-up">Get Started</Button>
+  <Button size="lg" class="text-lg px-8" href="/sign-up">Get Started Free</Button>
   <Button size="lg" variant="outline" class="text-lg px-8" href="/services">Learn More</Button>
 </div>
