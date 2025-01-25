9a9dfd308fe153f9d9fc226953271b91e92c3673
Nate Houk <nate@epsilonrecords.com>
2025-01-25 19:16:15 +0100

    Refactor

--- src/routes/api/enrich-data/+server.ts
+++ src/routes/api/enrich-data/+server.ts
@@ -1,4 +1,9 @@
 import { enrichData } from './enrich.js';
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+  runtime: 'nodejs20.x',
+};
 
 export async function GET() {
   return await enrichData();
