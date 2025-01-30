08e8933179c9a568a390792f149ba55889fc8d26
Nate Houk <nate@epsilonrecords.com>
2025-01-30 08:51:15 +0100

    refactor: ♻️ logging

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import logger from '../utils/logger';
+import logger from '../utils/logger.js';
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 if (!process.env.DATABASE_URL) {
