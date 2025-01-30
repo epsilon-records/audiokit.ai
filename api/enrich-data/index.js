ee47ffa4803ab80637de7928540cf2f5c6f662a8
Nate Houk <nate@epsilonrecords.com>
2025-01-30 08:56:52 +0100

    refactor: ♻️ logging

--- api/enrich-data/index.js
+++ api/enrich-data/index.js
@@ -2,5 +2,5 @@ import { enrichData } from '../../src/lib/server/enrich.js';
 
 export async function GET() {
   const result = await enrichData();
-  return new Response(result, { status: 200 });
+  return new Response(JSON.stringify(result), { status: 200 });
 }
